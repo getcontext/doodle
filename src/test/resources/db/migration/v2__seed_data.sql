bb2bdc1ac7d7b6695f8dba773e8123fe49cad695
wizard <andrzej.salamon@gmail.com>
2026-02-02 19:38:58 +0100

    begin adding sample data for tests using flyway

---
+++ src/test/resources/db/migration/v2__seed_data.sql
@@ -0,0 +1,5 @@
+-- Seed a handful of calendars that your tests can read
+INSERT INTO calendar (name, owner_id) VALUES
+    ('IT Calendar',     '11111111-1111-1111-1111-111111111111'),
+    ('Marketing Calendar', '22222222-2222-2222-2222-222222222222'),
+    ('HR Calendar',        '33333333-3333-3333-3333-333333333333');
